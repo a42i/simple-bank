7545c65d92ce90d5d1a0c35442fef67a4d36072b
Aditya Caprihan <ac@a42i.net>
2026-06-05 20:33:15 +1000

    Implement the rest of the cli module and add tests

--- src/simple_bank/cli.py
+++ src/simple_bank/cli.py
@@ -1,8 +1,94 @@
-"""CLI for the simple bank processor."""
+"""CLI for the simple bank processor.
 
+This module takes a very simplistic approach to error handling in order to save some time; it raises
+an error and crashes. This would definitely not be something we should do in a real system, unless
+we're on the Beam.
+
+"""
+
+import sys
 from argparse import ArgumentParser
 from pathlib import Path
-import sys
+from typing import Literal, TextIO, cast
+
+from simple_bank import csv_codec
+from simple_bank.codec import Balance, InputError, Transaction
+from simple_bank.core import CompanyAccounts, Result
+
+
+class ProgramError(Exception):
+    """Generic exception raised on any error."""
+
+    pass
+
+
+def main() -> int:
+    args = _parser().parse_args()
+
+    with (
+        _open(args.input_balances_file) as input_balances,
+        _open(args.transactions_file) as input_transactions,
+        _open(args.output_balances_file, "w") as output_balances,
+    ):
+        exit_code = run(input_balances, input_transactions, output_balances)
+        if not exit_code:
+            print("Completed.")
+        return exit_code
+
+
+def run(
+    input_balances: TextIO, input_transactions: TextIO, output_balances: TextIO
+) -> int:
+
+    accounts = CompanyAccounts()
+
+    _read_input_balances(input_balances, accounts)
+    _apply_input_transactions(input_transactions, accounts)
+
+    balances = (Balance(account, money) for (account, money) in accounts.balances())
+    csv_codec.write_balances(output_balances, balances)
+
+    return 0
+
+
+def _apply_input_transactions(input: TextIO, accounts: CompanyAccounts) -> None:
+    for txn in csv_codec.read_transactions(input):
+        match txn:
+            case Transaction(src, dest, amount):
+                match accounts.transfer(src, dest, amount):
+                    case Result.OK:
+                        pass
+                    case Result.INSUFFICIENT_BALANCE:
+                        raise ProgramError(
+                            f"(input transactions):transfer:insufficient {src=}, {dest=}, {amount=}"
+                        )
+                    case (Result.UNKNOWN_ACCOUNT, account):
+                        raise ProgramError(
+                            f"(input transactions):transfer:unknown {account=}"
+                        )
+
+            case InputError(line, msg):
+                raise ProgramError(f"(input transactions):read:{line=}, {msg=}")
+
+
+def _read_input_balances(input: TextIO, accounts: CompanyAccounts) -> None:
+    for balance in csv_codec.read_balances(input):
+        match balance:
+            case Balance(account, amount):
+                match accounts.init_balance(account, amount):
+                    case Result.OK:
+                        pass
+                    case Result.DUPLICATE:
+                        raise ProgramError(
+                            f"(input balances):init:duplicate {account=}"
+                        )
+
+            case InputError(line, msg):
+                raise ProgramError(f"(input balances):read:{line=}, {msg=}")
+
+
+def _open(path: Path, mode: Literal["r", "w"] = "r") -> TextIO:
+    return cast(TextIO, open(path, mode, newline="", encoding="utf-8"))
 
 
 def _parser() -> ArgumentParser:
@@ -32,12 +118,6 @@ def _parser() -> ArgumentParser:
     )
 
     return parser
-
-
-def main() -> int:
-    args = _parser().parse_args()
-
-    return 0
 
 
 if __name__ == "__main__":
---
+++ tests/test_cli.py
@@ -0,0 +1,113 @@
+from collections.abc import Iterable
+import csv
+from io import StringIO
+from typing import TextIO
+import unittest
+
+from simple_bank import cli, csv_codec
+from simple_bank.codec import Transaction, Balance
+from simple_bank.core import Account, Money
+
+
+class TestCLIRun(unittest.TestCase):
+    @staticmethod
+    def _invert_transactions(txns: Iterable[Transaction]) -> Iterable[Transaction]:
+        """Returns the "inverse" of a set of transactions.
+
+        i.e. the sequence of transactions, that reverses the affect of `txns`.
+
+        """
+        for src, dest, amount in reversed(list(txns)):
+            yield Transaction(dest, src, amount)
+
+    @staticmethod
+    def _read_transactions(input: TextIO) -> Iterable[Transaction]:
+        for txn in csv_codec.read_transactions(input):
+            match txn:
+                case Transaction():
+                    yield txn
+                case _:
+                    pass
+
+    @staticmethod
+    def _write_transactions(output: TextIO, txns: Iterable[Transaction]) -> None:
+        writer = csv.writer(output)
+        for src, dest, amount in txns:
+            writer.writerow(
+                (
+                    Account.serialise(src),
+                    Account.serialise(dest),
+                    Money.serialise(amount),
+                )
+            )
+
+    @staticmethod
+    def _read_balances(input: TextIO) -> Iterable[Balance]:
+        for balance in csv_codec.read_balances(input):
+            match balance:
+                case Balance():
+                    yield balance
+                case _:
+                    pass
+
+    def test_apply_then_reverse_transactions(self) -> None:
+        # Verify that we end up with the same balances when we apply some transactions, and then apply
+        # their inverse.
+        #
+        # This is also an excellent candicate for a property based test, but that is likely overkill
+        # for this exercise.
+
+        # The inputs for this test are from the coding challenge bundle.
+
+        input_balances = StringIO(
+            "\n".join(
+                [
+                    "1111234522226789,5000.00",
+                    "1111234522221234,10000.00",
+                    "2222123433331212,550.00",
+                    "1212343433335665,1200.00",
+                    "3212343433335755,50000.00",
+                ]
+            )
+        )
+
+        input_transactions = StringIO(
+            "\n".join(
+                [
+                    "1111234522226789,1212343433335665,500.00",
+                    "3212343433335755,2222123433331212,1000.00",
+                    "3212343433335755,1111234522226789,320.50",
+                    "1111234522221234,1212343433335665,25.60",
+                ]
+            )
+        )
+
+        # Apply `input_transactions` to `input_balances` to get the `output_balances`.
+        output_balances = StringIO()
+        _ = cli.run(input_balances, input_transactions, output_balances)
+
+        # Generate inverse transactions and write them to `reversed_transactions`.
+        _ = input_transactions.seek(0)
+        reversed_transactions = StringIO()
+        self._write_transactions(
+            reversed_transactions,
+            self._invert_transactions(
+                self._read_transactions(input_transactions)
+            ),
+        )
+
+        # Apply `reversed_transactions` to `output_balances` to get `next_balances`.
+        _ = output_balances.seek(0)
+        _ = reversed_transactions.seek(0)
+        next_balances = StringIO()
+        _ = cli.run(output_balances, reversed_transactions, next_balances)
+
+        # Now, `input_balances` and `next_balances` "should" be the same if there aren't any bugs in
+        # our code.
+        _ = input_balances.seek(0)
+        _ = next_balances.seek(0)
+
+        self.assertDictEqual(
+            dict(self._read_balances(input_balances)),
+            dict(self._read_balances(next_balances)),
+        )
