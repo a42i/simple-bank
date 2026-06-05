4b9a69e751fb2ace4ee049fb725afcd54d5975c8
Aditya Caprihan <ac@a42i.net>
2026-06-05 10:36:33 +1000

    Add Money dataclass and associated tests

--- src/simple_bank/core.py
+++ src/simple_bank/core.py
@@ -9,8 +9,10 @@ sensible action for the caller may be to terminate the program.
 # Also see https://docs.python.org/3/reference/compound_stmts.html#annotations
 from __future__ import annotations
 
+import decimal
 import re
 from dataclasses import dataclass
+from decimal import Decimal
 from typing import ClassVar, final
 
 
@@ -43,3 +45,59 @@ class Account:
         The returned value is always parse-able by `parse`.
         """
         return account._value
+
+
+@final
+@dataclass(frozen=True)
+class Money:
+    """Immutable Money implementation."""
+
+    _amount: Decimal
+
+    _PATTERN: ClassVar[re.Pattern[str]] = re.compile(r"\d+\.\d{2}")
+    _SENTINEL: ClassVar[object] = object()
+    _CENT: ClassVar[Decimal] = Decimal("0.01")  # used for quantization only
+
+    # Setup our own context so we can trap most errors, but still use half-even rounding. It
+    # is better to fail than give bad results.
+    #
+    # Refer to https://docs.python.org/3/library/decimal.html#decimal.BasicContext
+    _CONTEXT: ClassVar[decimal.Context] = decimal.BasicContext.copy()
+    _CONTEXT.rounding = decimal.ROUND_HALF_EVEN
+
+    def __init__(self, _amount: Decimal, _sentinel: object = None) -> None:
+        if _sentinel is not Money._SENTINEL:
+            raise TypeError("Use Money.parse() to create Money instances")
+        object.__setattr__(self, "_amount", _amount.quantize(Money._CENT))
+
+    @staticmethod
+    def add(x: Money, y: Money) -> Money:
+        """Return a new `Money` object with a value of `x + y`."""
+        result = Money._CONTEXT.add(x._amount, y._amount)
+        return Money(result, Money._SENTINEL)
+
+    @staticmethod
+    def subtract(x: Money, y: Money) -> Money | None:
+        """Return a new `Money` object with a value of `x - y`.
+
+        Since there isn't such a thing as "negative" money, return `None` if `y` is greater than `x`.
+        """
+        result = Money._CONTEXT.subtract(x._amount, y._amount)
+        if result.is_signed():
+            return None
+        return Money(result, Money._SENTINEL)
+
+    @staticmethod
+    def parse(value: str) -> Money | None:
+        """Parse `value` into Money, if possible."""
+        if Money._PATTERN.fullmatch(value):
+            return Money(Decimal(value), Money._SENTINEL)
+        return None
+
+    @staticmethod
+    def serialise(money: Money) -> str:
+        """Return a string representation of the Money object.
+
+        The returned value is always parse-able by `parse`.
+        """
+        return str(money._amount)
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,8 +1,10 @@
 # NOTE: Raw asserts are to keep the typechecker happy only.
 
 import unittest
+from decimal import Decimal
 
-from simple_bank.core import Account
+from simple_bank.core import Account, Money
+
 
 def _mk_account(value: str) -> Account:
     """Internal helper that assumes `value` is always valid."""
@@ -10,8 +12,15 @@ def _mk_account(value: str) -> Account:
     assert account
     return account
 
-class TestAccount(unittest.TestCase):
 
+def _mk_money(value: str) -> Money:
+    """Internal helper that assumes `value` is always valid."""
+    money = Money.parse(value)
+    assert money
+    return money
+
+
+class TestAccount(unittest.TestCase):
     # There is little value in trying to test that the `re` module works as advertised; focus on
     # ensuring we are using the "right" pattern.
 
@@ -41,3 +50,96 @@ class TestAccount(unittest.TestCase):
     def test_constructor_fails(self) -> None:
         with self.assertRaises(TypeError):
             _ = Account("0123456789123456")
+
+
+class TestMoney(unittest.TestCase):
+    # As above, focus on simple canary tests to ensure we haven't made any easily avoidable mistakes.
+
+    def test_parse_invalid(self) -> None:
+        cases = [
+            "",
+            "1",                # nothing after decimal
+            "1.0",              # < 2 digits after decimal
+            "1.000",            # > 2 digits after decimal
+            "-1.00",            # signed value
+            "+1.00",            # signed value
+            "1.a0",             # non-numeric
+            ".00",              # nothing before the decimal
+        ]
+
+        for case in cases:
+            with self.subTest("f{case=}"):
+                self.assertIsNone(Money.parse(case))
+
+    def test_parse_valid(self) -> None:
+        cases = [
+            "0.00",
+            "1.05",
+            "100.99",
+        ]
+
+        for case in cases:
+            with self.subTest(f"{case=}"):
+                parsed = _mk_money(case)
+
+                self.assertEqual(case, Money.serialise(parsed))
+
+    def test_serialise(self) -> None:
+        parsed = _mk_money("10.99")
+
+        self.assertEqual(parsed, Money.parse(Money.serialise(parsed)))
+
+    def test_add(self) -> None:
+        cases = [
+            # (a, b, c = a + b)
+            ("1.00", "0.00", "1.00"),
+            ("0.00", "1.00", "1.00"),
+            ("1.00", "2.00", "3.00"),
+            ("99.99", "0.01", "100.00"),
+        ]
+
+        for val_a, val_b, val_c in cases:
+            with self.subTest(f"{val_a=}, {val_b=}"):
+                a = _mk_money(val_a)
+                b = _mk_money(val_b)
+
+                c = Money.add(a, b)
+                assert c is not None
+
+                self.assertEqual(val_c, Money.serialise(c))
+
+    def test_subtract_valid(self) -> None:
+        cases = [
+            # (a, b, c = a - b : a > b)
+            ("1.00", "0.00", "1.00"),
+            ("1.00", "1.00", "0.00"),
+            ("3.00", "2.00", "1.00"),
+            ("100.00", "0.01", "99.99"),
+        ]
+
+        for val_a, val_b, val_c in cases:
+            with self.subTest(f"{val_a=}, {val_b=}"):
+                a = _mk_money(val_a)
+                b = _mk_money(val_b)
+
+                c = Money.subtract(a, b)
+                assert c is not None
+
+                self.assertEqual(val_c, Money.serialise(c))
+
+    def test_subtract_invalid(self) -> None:
+        cases = [
+            ("0.00", "0.01"),
+            ("1.00", "1.01"),
+        ]
+
+        for val_a, val_b in cases:
+            with self.subTest(f"{val_a=}, {val_b=}"):
+                a = _mk_money(val_a)
+                b = _mk_money(val_b)
+
+                self.assertIsNone(Money.subtract(a, b))
+
+    def test_constructor_raises(self) -> None:
+        with self.assertRaises(TypeError):
+            _ = Money(Decimal("10.00"))
